271dbc1401386ae3e73bea9a41bc4bcb6841e146
Menosuno02 <mainusone1@gmail.com>
2024-01-12 15:15:20 +0100

    Commit

--- proyectotechriders/src/app/components/proponer-tecnologia/proponer-tecnologia.component.ts
+++ proyectotechriders/src/app/components/proponer-tecnologia/proponer-tecnologia.component.ts
@@ -1,32 +1,23 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ServicePrincipal } from 'src/app/services/service.principal';
 @Component({
   selector: 'app-proponer-tecnologia',
   templateUrl: './proponer-tecnologia.component.html',
-  styleUrls: ['./proponer-tecnologia.component.css']
+  styleUrls: ['./proponer-tecnologia.component.css'],
 })
-export class ProponerTecnologiaComponent implements OnInit{
-constructor(private _router: Router,private _service: ServicePrincipal) {
-  
-}
+export class ProponerTecnologiaComponent implements OnInit {
+  constructor(private _router: Router, private _service: ServicePrincipal) {}
 
-public tecnologias!: any[];
-ngOnInit(): void {
-  
+  public tecnologias!: any[];
+  ngOnInit(): void {
     // TechRider
-    this._service
-      .getTipoTecnologias()
-      .subscribe((response) => {
-        this.tecnologias = response;
-      });
-  
-}
+    this._service.getTipoTecnologias().subscribe((response) => {
+      this.tecnologias = response;
+    });
+  }
   enviarSolicitud(): void {
-    
-    
-      this._router.navigate(['/usuario/editar-tecnologias']);
-      //al enviar la solicitud de nueva tecnologia es importante saber que el TiposPeticionesCategorias el rol es=4
-   
+    this._router.navigate(['/usuario/editar-tecnologias']);
+    //al enviar la solicitud de nueva tecnologia es importante saber que el TiposPeticionesCategorias el rol es=4
   }
 }
